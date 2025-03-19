bc075a09dd657f76542fff693274eb4de41115d7
yuvaraj-aiprofessional <yuvaraj.rajendrandp@gmail.com>
2025-03-19 14:31:44 +0530

    Assignment 02 - Functions - Submission

--- assignment01_CS_Q1.py
+++
@@ -1,5 +0,0 @@
-#Title - Assignment-1-Control Structures - Q1
-#To print the welcome message.
-#Author - Yuvaraj Rajendran
-#Date - 19/03/2025
-print("Welcome to Assignment-1")
--- assignment01_CS_Q2.py
+++
@@ -1,9 +0,0 @@
-#Title - Assignment-1-Control Structures - Q2
-#To print the two numbers with addition values
-#Author - Yuvaraj Rajendran
-#Date - 19/03/2025
-Num1=10; Num2=30;
-Add=Num1+Num2;
-print("Num1 = ",Num1)
-print("Num2 = ",Num2)
-print("Add = ",Add)
